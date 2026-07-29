f3c90f491ebeb3126e9bdcf58a1116ba19fc74f6
Saria Essid <saria.essid@esprit.tn>
2026-07-29 18:20:19 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My love is Missar Mamy Baba Kotkot . My Love is Missar!");
+        System.out.println("Invitation for MontaSSaria Wedding !");
     }
 }
 
